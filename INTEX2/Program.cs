10afd8c175edc43f9cc2c263d59e8d0284c16318
jakerg@byu.edu <jakerg@byu.edu>
2024-04-03 10:56:41 -0600

    Add project files.

---
+++ INTEX2/Program.cs
@@ -0,0 +1,100 @@
+using INTEX2.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var services = builder.Services;
+        var config = builder.Configuration;
+
+
+        // Add services to the container.
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+        builder.Services.AddControllersWithViews();
+
+        services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+        {
+            microsoftOptions.ClientId = config["Authentication:Microsoft:ClientId"];
+            microsoftOptions.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        //When we create a scope, we are able to access the services that we have configured in the identity
+        using (var scope = app.Services.CreateScope())
+        {
+            //Add your roles here (seeding some initial data into our system)
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "Admin", "Manager", "Member" };
+
+            foreach (var role in roles)
+            {
+                //Check if a role exists
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+        }
+
+        //It will be good to seed some default users
+        using (var scope = app.Services.CreateScope())
+        {
+            //Add your users here (seeding some initial data into our system)
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string email = "admin@admin.com";
+            string password = "Test1234,56789!";
+
+            //Seed a new admin user if there is no data available
+            if (await userManager.FindByEmailAsync(email) == null)
+            {
+                var user = new IdentityUser();
+                user.UserName = email;
+                user.Email = email;
+
+                await userManager.CreateAsync(user, password);
+
+                userManager.AddToRoleAsync()
+            }
+
+
+        }
+        app.Run();
+    }
+}
